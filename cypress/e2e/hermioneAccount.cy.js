dc38a56f158f94836ef3825305319e439ce5bbe4
Oleksandr Kovalchuk <kov280396@gmail.com>
2025-04-13 20:46:02 +0300

    fixed issues

--- cypress/e2e/hermioneAccount.cy.js
+++ cypress/e2e/hermioneAccount.cy.js
@@ -1,73 +1,43 @@
-import { faker } from '@faker-js/faker';
 /// <reference types='cypress' />
 
-describe('GlobalsQa Bank App - Hermione Granger', () => {
-  const user = 'Hermoine Granger';
-  const accountNumber = '1001';
-  const secondaryAccountNumber = '1002';
-  const currency = 'Dollar';
-
-  const depositAmount = `${faker.number.int({ min: 500, max: 1000 })}`;
-  const withdrawAmount = `${faker.number.int({ min: 50, max: 500 })}`;
-
-  const selectors = {
-    customerLoginBtn: '.btn:contains("Customer Login")',
-    loginBtn: '.btn:contains("Login")',
-    userDropdown: '[name="userSelect"]',
-    accountDropdown: '[name="accountSelect"]',
-    accountInfo: '[ng-hide="noAccount"]',
-    depositTab: '[ng-click="deposit()"]',
-    withdrawTab: '[ng-click="withdrawl()"]',
-    transactionTab: '[ng-click="transactions()"]',
-    backBtn: '[ng-click="back()"]',
-    logoutBtn: '[ng-click="byebye()"]',
-    amountInput: '[placeholder="amount"]',
-    submitBtn: '[type="submit"]',
-    depositMsg: '[ng-show="message"]',
-    transactionTable: 'table'
-  };
-
+describe('Bank app', () => {
   before(() => {
     cy.visit('/');
   });
 
-  it('should handle full flow from login to transactions', () => {
-    cy.get(selectors.customerLoginBtn).click();
-    cy.get(selectors.userDropdown).select(user);
-    cy.get(selectors.loginBtn).click();
+  it('should provide the ability to work with Hermione\'s bank account', () => {
+    cy.contains('button', 'Customer Login').click();
+    cy.get('#userSelect').select('Hermoine Granger');
+    cy.get('.btn-default').should('be.visible').click();
 
-    cy.get(selectors.accountInfo)
-      .contains('strong', accountNumber)
+    cy.contains('.center', 'Account Number')
+      .should('contain.text', '1001');
+    cy.contains('.center', 'Balance').should('contain.text', 5096);
+    cy.contains('.center', 'Currency').should('contain.text', 'Dollar');
+
+    cy.get('[ng-class="btnClass2"]').click();
+    cy.get('.form-control').type(1000);
+    cy.get('[type="submit"]').click();
+    cy.contains('[ng-show="message"]', 'Deposit Successful')
       .should('be.visible');
+    cy.contains('.center', 'Balance').should('contain.text', 6096);
 
-    cy.get(selectors.accountInfo).contains('strong', '0').should('be.visible');
+    cy.get('[ng-class="btnClass3"]').click();
+    cy.contains('[type="submit"]', 'Withdraw').should('be.visible');
+    cy.get('[placeholder="amount"]').type(500);
+    cy.contains('[type="submit"]', 'Withdraw').click();
+    cy.contains('[ng-show="message"]', 'Transaction successful')
+      .should('be.visible');
+    cy.contains('.center', 'Balance')
+      .should('contain.text', 5596);
+    cy.reload();
 
-    cy.contains('.ng-binding', currency).should('be.visible');
+    cy.get('[ng-click="transactions()"]').click();
+    cy.get('td.ng-binding').should('contain', 1000);
+    cy.get('td.ng-binding').should('contain', 500);
+    cy.get('[ng-click="back()"]').click();
 
-    cy.get(selectors.depositTab).click();
-    cy.get(selectors.amountInput).type(depositAmount);
-    cy.get(selectors.submitBtn).contains('Deposit').click();
-
-    cy.get(selectors.depositMsg).should('contain', 'Deposit Successful');
-
-    cy.get(selectors.accountInfo).contains('Balance').should('be.visible');
-
-    cy.get(selectors.withdrawTab).click();
-    cy.get(selectors.amountInput).type(withdrawAmount);
-    cy.get(selectors.submitBtn).contains('Withdraw').click();
-
-    cy.get(selectors.transactionTab).click();
-    cy.get(selectors.transactionTable).should('contain', 'Transaction Type');
-
-    cy.get(selectors.backBtn).click();
-    cy.get(selectors.accountDropdown).select(secondaryAccountNumber);
-    cy.get(selectors.transactionTab).click();
-
-    cy.get(selectors.transactionTable)
-      .should('not.contain', 'Deposit')
-      .and('not.contain', 'Withdraw');
-
-    cy.get(selectors.backBtn).click();
-    cy.get(selectors.logoutBtn).click();
+    cy.get('.logout').click();
+    cy.url().should('include', '/customer');
   });
 });
